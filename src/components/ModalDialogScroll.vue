b55d02c7d1a7d41d571ab58e760a358dadc39965
satrio92 <kukuhsatrio920@gmail.com>
2022-04-12 12:57:12 +0700

    another commit

--- src/components/ModalDialogScroll.vue
+++ src/components/ModalDialogScroll.vue
@@ -1,11 +1,11 @@
 <template>
   <div
-    v-if="modals"
+    v-if="modal"
     class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full"
     id="my-modal"
   ></div>
   <div
-    v-if="modals"
+    v-if="modal"
     class="fixed inset-0 flex justify-center items-center w-full h-full py-12 px-5 md:px-0"
   >
     <div class="md:w-1/2 bg-white h-full overflow-hidden">
@@ -13,7 +13,7 @@
         class="px-10 py-5 shadow-md text-gray-700 font-semibold bg-white flex items-center justify-between"
       >
         <span>TERMS AND CONDITION</span>
-        <i class="bi bi-x text-2xl font-bold" @click="this.modal = false"></i>
+        <i class="bi bi-x text-2xl font-bold" @click="modals = false"></i>
       </div>
       <div
         class="text-gray-500 tex-sm px-10 pt-5 pb-24 text-justify flex flex-col gap-3 h-full overflow-y-scroll"
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,15 @@
 <template>
   <Navbar />
-  <div id="about" class="font-poppins flex justify-center py-12 bg-base-200">
-        <h1 class="text-2xl font-bold">Ini Halaman About</h1>
+  <div id="about" class="font-poppins flex flex-col items-center py-12 gap-4 bg-base-200 px-24">
+    <div class="flex gap-4">
+      <div class="flex flex-col gap-4 w-1/2">
+        <h1 class="text-2xl font-bold">Tentang Rollaas Cafe</h1>
+        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. In non dignissimos cupiditate similique rem vero sequi deserunt quasi iure assumenda nesciunt, placeat harum eum tempore iusto alias labore eos eaque amet, possimus enim. Ullam blanditiis eius iure, unde dolores itaque odio amet ratione eum voluptatibus ea nobis debitis distinctio quae soluta harum asperiores pariatur quibusdam tempore? Quasi exercitationem dolorem architecto velit natus iure asperiores illo quos! Tenetur totam minima neque quibusdam vero excepturi labore possimus. Nulla qui vitae architecto harum.</p>
+      </div>
+      <div class="bg-gradient-to-r from-[#9E7B4B] to-[#CDA976] w-2/5 absolute right-0">
+        <img src="../assets/illustration.png" alt="Gambar Rollaas Cafe">
+      </div>
+    </div>
     </div>
 </template>
 
